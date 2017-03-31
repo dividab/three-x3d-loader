85f9298ede718882772d13684cffc6b8b0b2c6be
Adam Luotonen <adam.luotonen@divid.se>
2017-03-31 16:36:56 +0200

    isomorphic

--- src/x3d-loader.js
+++ src/x3d-loader.js
@@ -1,4 +1,5 @@
 var renderX3D = require('./x3d-parser');
+var DOMParser = require('xmldom').DOMParser;
 
 module.exports = function (THREE) {
 
@@ -47,23 +48,9 @@ module.exports = function (THREE) {
 
 		parse: function (data, scene) {
 
-			var parseXml = function (xmlStr) {
-				if (typeof window.DOMParser != "undefined") {
-					return ( new window.DOMParser() ).parseFromString(xmlStr, "text/xml");
-				} else if (typeof window.ActiveXObject != "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
-					var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
-					xmlDoc.async = "false";
-					xmlDoc.loadXML(xmlStr);
-					return xmlDoc;
-				} else {
-					throw new Error("No XML parser found");
-				}
-			};
-
-			//var texturePath = this.texturePath || '';
-
+			var parser = new DOMParser();
 			var scene = new THREE.Scene();
-			var x3dXml = parseXml(data);
+			var x3dXml = parser.parseFromString(data);
 			renderX3D(THREE, x3dXml, scene);
 
 			return scene;
